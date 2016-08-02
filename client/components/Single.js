e50e89bd915f79386394d39ad449987e5163a61a
savioabuga <savioabuga@gmail.com>
2016-08-02 09:46:25 +0300

    missing files

---
+++ client/components/Single.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+
+
+export default class Single extends Component {
+  render() {
+    // index of the post
+    const { postId } = this.props.params
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+
+    // give us the post
+    const post = this.props.posts[i];
+
+    const postComments = this.props.comments[postId] || [];
+
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments {...this.props} postComments={postComments}/>
+      </div>
+    );
+  }
+}
